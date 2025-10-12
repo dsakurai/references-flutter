b231ded7bca070a553dc39c7401b8e707a45da42
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-10-12 04:33:21 +0000

    Add pub-get script to automate dependency installation in subdirectories

---
+++ pub-get.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Running pub get in subdirectories..."
+
+echo "Running pub get in directory $PWD/server..."
+    (cd server && dart pub get)
+echo ""
+
+echo "Running pub get in directory $PWD/references_models..."
+    (cd references_models && dart pub get)
+echo ""
+
+echo "Running pub get in directory $PWD/webapp..."
+    (cd webapp && flutter pub get)
+echo ""
